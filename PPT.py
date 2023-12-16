babd8712346739dfa803b7786a45f2ad0745cec9
rochelAsc <rochelinho@gmail.com>
2023-12-16 00:32:52 -0300

    16/12 - CONTADOR ADICIONADO

--- PPT.py
+++ PPT.py
@@ -3,32 +3,42 @@ import sys
 import pygame
 import random
 
-
 dirpath = os.getcwd()
 sys.path.append(dirpath)
 if getattr(sys, "frozen", False):
     os.chdir(sys._MEIPASS)
-
 
 pygame.init()
 
 '#CONSTANTES'
 tela_altura = 500
 tela_largura = 500
-claro = (223, 247, 242)
-cinza = (204, 222, 227)
-preto = (0, 0, 0)
-branco = (255, 255, 255)
-mercurio = (196, 222, 255)
-prata = (182, 207, 222)
-opcoes = ['pedra', 'papel', 'tesoura']
-escolha_jogador = []
+claro = (223, 247, 242)  # PALETA DE COR
+cinza = (204, 222, 227)  # PALETA DE COR
+preto = (0, 0, 0)  # PALETA DE COR
+branco = (255, 255, 255)  # PALETA DE COR
+mercurio = (196, 222, 255)  # PALETA DE COR
+prata = (182, 207, 222)  # PALETA DE COR
+opcoes = ['pedra', 'papel', 'tesoura', 'pedra', 'papel', 'tesoura', 'pedra', 'papel', 'tesoura', 'pedra', 'papel', 'tesoura']
+escolha_jogador = []  # LISTA, A OPÇÃO DO JOGADOR É INCREMENTADA AO ESCOLHER
+resultado = None  # FLAG PARA IDENTIFICAR SE VOCÊ VENCEU, PERDEU OU DEU EMPATE
+score = 0  # RECORDE, HIGHSCORE, SCORE, ETC
 '#MAIN//DATA'
-gamelogo = pygame.image.load("data/gamelogo2.png")
+gamelogo = pygame.image.load("data/gamelogo2.png")  # LOGO
 pygame.display.set_icon(gamelogo)
 TELA_DISPLAY = pygame.display.set_mode([tela_altura, tela_largura])
-pygame.display.set_caption("PEDRA.. PAPEL... TESOURA!!!")
+pygame.display.set_caption("PEDRA.. PAPEL... TESOURA!!!")  # NOME
 fps = pygame.time.Clock()
+pygame.font.init()
+
+
+def message_display(texto, fonte, tamanho, cor, x, y, antialias=True):
+    """CRIA UMA MENSAGEM, DADO O TEXTO, A FONTE, TAMANHO, COR E COORDENADAS"""
+    # ANTILIAS É O 'MODO' DE RENDERIZAÇÃO DO TEXTO
+    pptfont = pygame.font.SysFont(fonte, tamanho)
+    frase = pptfont.render(texto, antialias, cor)
+    TELA_DISPLAY.blit(frase, (x, y))
+    # pygame.display.update()
 
 
 def musica(som, loops):
@@ -69,55 +79,8 @@ def playerbutton(arquivo, arquivo2, largura, altura, x, y, palavra, act=None):
         icone(arquivo, x, y)
 
 
-def frontpage():
-    """TELA INICIAL"""
-    musica("data/menuiniciar.wav", -1)
-    while frontpage:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-        TELA_DISPLAY.fill(cinza)
-        icone("data/NAME.png", 72, 40)
-        button("data/SAIR.png", "data/SAIR_H.png", 52, 19, 220, 315, sys.exit)
-        button("data/JOGAR.png", "data/JOGAR_H.png", 134, 35, 180, 250, homepage)
-        pygame.display.update()
-        fps.tick(30)
-
-
-def homepage():
-    """PAGINA PRINCIPAL"""
-    musica("data/menujogar.wav", -1)
-    gameloop = True
-    while gameloop:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-        TELA_DISPLAY.fill(cinza)
-        icone("data/e_sab.png", 92, 340)
-        button("data/VOLTAR.png", "data/VOLTAR2.png", 78, 17, 15, 30, frontpage)
-        playerbutton("data/PEDRA_01.png", "data/PEDRA_02.png", 74, 78, 55, 120, "pedra", adversario)
-        playerbutton("data/TESOURA_01.png", "data/TESOURA_02.png", 39, 62, 395, 124, "tesoura", adversario)
-        playerbutton("data/PAPEL_01.png", "data/PAPEL_02.png", 74, 74, 215, 120, "papel", adversario)
-        pygame.display.update()
-        fps.tick(30)
-
-
-def adversario():
-    """ESCOLHA DO COMPUTADOR"""
-    escolha_adversario = random.choice(opcoes)
-    if escolha_adversario == escolha_jogador[0]:
-        empate()
-    elif (escolha_jogador[0] == 'pedra' and escolha_adversario == 'tesoura' or
-          escolha_jogador[0] == 'papel' and escolha_adversario == 'pedra' or
-          escolha_jogador[0] == 'tesoura' and escolha_adversario == 'papel'):
-        vitoria()
-    elif (escolha_jogador[0] == 'pedra' and escolha_adversario == 'papel' or
-          escolha_jogador[0] == 'papel' and escolha_adversario == 'tesoura' or
-          escolha_jogador[0] == 'tesoura' and escolha_adversario == 'pedra'):
-        derrota()
-
-
 def empate():
+    global score, resultado
     """TELA DE EMPATE"""
     musica("data/cinemasins.wav", 1)
     delay = pygame.time.get_ticks() + 750
@@ -128,8 +91,9 @@ def empate():
             if event.type == pygame.QUIT:
                 pygame.quit()
         TELA_DISPLAY.fill(cinza)
-        if taxa >= delay-400:
+        if taxa >= delay - 400:
             icone("data/tie.png", 122, 215)
+            resultado = 'empate'
         if taxa >= delay + (750 * 4):
             homepage()
         pygame.display.update()
@@ -137,6 +101,7 @@ def empate():
 
 
 def vitoria():
+    global score, resultado
     """TELA DE VITORIA"""
     musica("data/success.wav", 2)
     delay = pygame.time.get_ticks() + 750
@@ -149,7 +114,8 @@ def vitoria():
         TELA_DISPLAY.fill(branco)
         if taxa >= delay:
             icone("data/VITORIA.png", 122, 215)
-        if taxa >= delay+(750 * 2):
+            resultado = 'vitoria'
+        if taxa >= delay + (750 * 2):
             icone("data/score.png", 347, 210)
             icone("data/score.png", (347 + 19), 210)
             icone("data/score.png", (347 + (19 * 2)), 210)
@@ -160,6 +126,7 @@ def vitoria():
 
 
 def derrota():
+    global score, resultado
     """TELA DE DERROTA"""
     musica("data/wasted.wav", 1)
     delay = pygame.time.get_ticks() + 750
@@ -170,8 +137,9 @@ def derrota():
             if event.type == pygame.QUIT:
                 pygame.quit()
         TELA_DISPLAY.fill(preto)
-        if taxa >= delay+1530:
+        if taxa >= delay + 1530:
             icone("data/DERROTA.png", 125, 230)
+            resultado = 'derrota'
         if taxa >= delay + (1175 * 2):
             icone("data/dot.png", 360, 270)
         if taxa >= delay + (1175 * 3):
@@ -179,12 +147,74 @@ def derrota():
         if taxa >= delay + (1175 * 4):
             icone("data/dot.png", 360 + (14 * 2), 270)
         if taxa >= delay + (1225 * 5):
-            frontpage()
+            homepage()
         pygame.display.update()
         fps.tick(30)
 
 
-frontpage()
-homepage()
-#sys.exit()
+def main():
+    """FUNÇÃO PRINCIPAL"""
 
+
+while main:
+
+    def frontpage():
+        """TELA INICIAL"""
+        musica("data/menuiniciar.wav", -1)
+        while frontpage:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+            TELA_DISPLAY.fill(cinza)
+            icone("data/NAME.png", 72, 40)
+            button("data/SAIR.png", "data/SAIR_H.png", 52, 19, 220, 315, sys.exit)
+            button("data/JOGAR.png", "data/JOGAR_H.png", 134, 35, 180, 250, homepage)
+            pygame.display.update()
+            fps.tick(30)
+
+
+    def homepage():
+        global score, resultado
+        """PAGINA PRINCIPAL"""
+        # score = 0
+        if resultado == 'vitoria':
+            score += 1
+        elif resultado == 'derrota':
+            score = 0
+        elif resultado == 'empate':
+            score += 0
+        musica("data/menujogar.wav", -1)
+        while homepage:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+            TELA_DISPLAY.fill(cinza)
+            icone("data/e_sab.png", 92, 340)
+            button("data/VOLTAR.png", "data/VOLTAR2.png", 78, 17, 15, 30, frontpage)
+            playerbutton("data/PEDRA_01.png", "data/PEDRA_02.png", 74, 78, 55, 120, "pedra", adversario)
+            playerbutton("data/TESOURA_01.png", "data/TESOURA_02.png", 39, 62, 395, 124, "tesoura", adversario)
+            playerbutton("data/PAPEL_01.png", "data/PAPEL_02.png", 74, 74, 215, 120, "papel", adversario)
+            message_display(f"Highscore: {score}", 'arial', 22, preto, 205, 50, antialias=True)
+            pygame.display.update()
+            fps.tick(30)
+
+
+    def adversario():
+        """ESCOLHA DO COMPUTADOR"""
+        escolha_adversario = random.choice(opcoes)
+        if escolha_adversario == escolha_jogador[0]:
+            empate()
+        elif (escolha_jogador[0] == 'pedra' and escolha_adversario == 'tesoura' or
+              escolha_jogador[0] == 'papel' and escolha_adversario == 'pedra' or
+              escolha_jogador[0] == 'tesoura' and escolha_adversario == 'papel'):
+            vitoria()
+        elif (escolha_jogador[0] == 'pedra' and escolha_adversario == 'papel' or
+              escolha_jogador[0] == 'papel' and escolha_adversario == 'tesoura' or
+              escolha_jogador[0] == 'tesoura' and escolha_adversario == 'pedra'):
+            derrota()
+
+
+    frontpage()
+    homepage()
+
+main()
